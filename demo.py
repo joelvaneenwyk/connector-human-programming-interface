c50a1672505d27d15ae5e1f0c9f0bb1d871018c6
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2025-01-06 13:44:13 -0800

    refactor: enhance demo.py by improving path handling, adding context manager for temporary directories, and updating annotations
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- demo.py
+++ demo.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python3
-from subprocess import check_call, DEVNULL
-from shutil import copytree, ignore_patterns
 import os
-from os.path import abspath
-from sys import executable as python
+from contextlib import contextmanager
+
 from pathlib import Path
+from shutil import copytree, ignore_patterns, rmtree
+from subprocess import DEVNULL, check_call
+from sys import executable as python
+import tempfile
 
 my_repo = Path(__file__).absolute().parent
 
 
 def run() -> None:
+    """
+    running should result in something like this:
+
+    URL:   https://tacticaltech.org/
+    Title: Tactical Technology Collective
+    1 annotations
+
+    URL:   https://web.hypothes.is/blog/annotating-the-wild-west-of-information-flow/
+    Title: Annotating the wild west of information flow - Hypothesis
+    1 annotations
+
+    URL:   http://www.liberation.fr/futurs/2016/12/12/megafichier-beauvau-prie-de-revoir-sa-copie_1534720
+    Title: «Mégafichier»: Beauvau prié de revoir sa copie
+    3 annotations
+
+    URL:   https://www.wired.com/2016/12/7500-faceless-coders-paid-bitcoin-built-hedge-funds-brain/
+    Title: 7,500 Faceless Coders Paid in Bitcoin Built a Hedge Fund's Brain
+    4 annotations
+
+    URL:   http://realscreen.com/2016/12/06/project-x-tough-among-sundance-17-doc-shorts/
+    Title: “Project X,” “Tough” among Sundance '17 doc shorts
+    1 annotations
+
+    URL:   https://grehack.fr/2016/program
+    Title: GreHack | Security conference and hacking game 2016 | Grenoble
+    1 annotations
+
+    URL:   https://respectmynet.eu/
+    Title: [!] Respect My Net
+    1 annotations
+
+    URL:   https://www.youtube.com/watch?v=Xgp7BIBtPhk
+    Title: BBC Documentaries 2016: The Joy of Data [FULL BBC SCIENCE DOCUMENTARY]
+    1 annotations
+    """
+
     # uses fixed paths; worth it for the sake of demonstration
     # assumes we're in /tmp/my_demo now
 
@@ -27,27 +65,28 @@ def run() -> None:
         check_call(f'{python} -m pip install        git+https://github.com/karlicoss/hypexport.git'.split())
     else:
         try:
-            import hypexport
+            import hypexport  # pylint: disable=unused-import,import-outside-toplevel
         except ModuleNotFoundError:
             check_call(f'{python} -m pip --user git+https://github.com/karlicoss/hypexport.git'.split())
 
 
     # 3. prepare some demo Hypothesis data
-    hypothesis_backups = Path.resolve('backups/hypothesis')
-    Path(hypothesis_backups).mkdir(exist_ok=True, parents=True)
+    hypothesis_backups = Path('backups/hypothesis').resolve()
+    hypothesis_backups.mkdir(exist_ok=True, parents=True)
     check_call([
         'curl',
         'https://raw.githubusercontent.com/taniki/netrights-dashboard-mockup/master/_data/annotations.json',
-        '-o', hypothesis_backups + '/annotations.json',
+        '-o', f'{hypothesis_backups}/annotations.json',
     ], stderr=DEVNULL)
     #
 
     # 4. point my.config to the Hypothesis data
-    mycfg_root = Path.resolve('my_repo')
-    init_file = Path(mycfg_root) / 'my/config.py'
+    mycfg_root = Path('my_repo').resolve()
+    mycfg_root.mkdir(exist_ok=True, parents=True)
+    init_file: Path = mycfg_root / 'my/config.py'
     init_file.write_text(init_file.read_text().replace(
         '/path/to/hypothesis/data',
-        hypothesis_backups,
+        str(hypothesis_backups),
     ))
     #
 
@@ -68,63 +107,26 @@ for page in islice(pages, 0, 8):
 '''], env={
     # this is just to prevent demo.py from using real data
     # normally, it will rely on having my.config in ~/.config/my
-    'MY_CONFIG': mycfg_root,
+    'MY_CONFIG': str(mycfg_root),
     **os.environ,
 })
 
-"""
-that should result in something like this:
 
-URL:   https://tacticaltech.org/
-Title: Tactical Technology Collective
-1 annotations
-
-URL:   https://web.hypothes.is/blog/annotating-the-wild-west-of-information-flow/
-Title: Annotating the wild west of information flow - Hypothesis
-1 annotations
-
-URL:   http://www.liberation.fr/futurs/2016/12/12/megafichier-beauvau-prie-de-revoir-sa-copie_1534720
-Title: «Mégafichier»: Beauvau prié de revoir sa copie
-3 annotations
-
-URL:   https://www.wired.com/2016/12/7500-faceless-coders-paid-bitcoin-built-hedge-funds-brain/
-Title: 7,500 Faceless Coders Paid in Bitcoin Built a Hedge Fund's Brain
-4 annotations
-
-URL:   http://realscreen.com/2016/12/06/project-x-tough-among-sundance-17-doc-shorts/
-Title: “Project X,” “Tough” among Sundance '17 doc shorts
-1 annotations
-
-URL:   https://grehack.fr/2016/program
-Title: GreHack | Security conference and hacking game 2016 | Grenoble
-1 annotations
-
-URL:   https://respectmynet.eu/
-Title: [!] Respect My Net
-1 annotations
-
-URL:   https://www.youtube.com/watch?v=Xgp7BIBtPhk
-Title: BBC Documentaries 2016: The Joy of Data [FULL BBC SCIENCE DOCUMENTARY]
-1 annotations
-"""
-
-from contextlib import contextmanager
 @contextmanager
 def named_temp_dir(name: str):
     """
-    Fixed name tmp dir
+    Create a unique temporary directory and return a path that includes the specified name.
     """
-    import tempfile
-    td = Path(tempfile.gettempdir()) / name
+    unique_temp_dir = Path(tempfile.mkdtemp())
+    td = unique_temp_dir / name
     try:
-        td.mkdir(exist_ok=False)
+        td.mkdir()
         yield td
     finally:
-        import os, shutil
-        skip_cleanup = 'CI' in os.environ and os.name == 'nt'
-        # TODO hmm for some reason cleanup on windows causes AccessError
-        if not skip_cleanup:
-            shutil.rmtree(str(td))
+        try:
+            rmtree(str(unique_temp_dir), ignore_errors=True)
+        except Exception as e:
+            print(f"Failed to cleanup {unique_temp_dir}: {e}")
 
 
 def main():
